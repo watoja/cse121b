f246122f93d809b4e591502b127fa2a0bdda45a1
watoja <jimwattsabraham@gmail.com>
2023-09-17 01:51:38 +0300

    adjustmentsw01

--- w01-task.js
+++ w01-task.js
@@ -10,7 +10,7 @@ console.log(`It is now ${currentDateAndTime}`);
 
 // 🔍 Part 3 error. The following statement calls a function named total that accepts any number of arguments and returns the sum. The returned value is stored in a variable named theTotal. 1-10 are the arguments.
 
-let theTotal = total(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+let theTotal = total(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11);
 console.log(`The total is ${theTotal}`);
 
 // 'total' function declaration
